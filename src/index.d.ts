f6465aaef657910242f4f68e3e05a4b5430a1580
Ben Dowen <dirtydevil@gmail.com>
2024-04-06 19:42:17 +0100

    fix typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,3 @@
-export function deck(numberOfDecks: number, shouldShuffle: number): Array<any>;
+export function deck(numberOfDecks?: number, shouldShuffle?: boolean): Array<any>;
 export function repeatDeck(deck: Array<any>, n: boolean): Array<any>;
 export function shuffleArray(array: Array<any>): Array<any>;
